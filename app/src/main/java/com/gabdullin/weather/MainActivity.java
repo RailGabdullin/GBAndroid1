89098df2f50e1779822e2146a7d7f77c85b08041
RailGabdullin <rail.gabdullin@gmail.com>
2018-12-03 02:12:49 +0300

    Временно добавлено отображение LifeCycle в Toast-ах и Logcat (в рамках дз), Добавлены зачатки интерфейса

--- app/src/main/java/com/gabdullin/weather/MainActivity.java
+++ app/src/main/java/com/gabdullin/weather/MainActivity.java
@@ -1,13 +1,65 @@
 package com.gabdullin.weather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String Tag = "LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeToast("onCreate");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        makeToast("onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        makeToast("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        makeToast("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy");
+    }
+
+    private void makeToast (String text){
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+        Log.i(Tag, text);
     }
 }
